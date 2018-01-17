a31784bec78bdb875c43f6250d1b96541e3b4b8d
Pavlo Korenivskiy <pavlokorenivskiy@Pavlos-MacBook-Pro.local>
2018-01-18 00:13:52 +0200

    initial on mac

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Vigenere;
 
 namespace CodeVigenere
 {
@@ -6,7 +7,10 @@ namespace CodeVigenere
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var cryptor = new  CryptVigenere();
+
+            cryptor.Encrypt("LEMON", "ATTACKATDAWN");
+            cryptor.Decrypt("LEMON", "LXFOPVEFRNHR");
         }
     }
 }
